7fae6f9d75ea604b37c904f435dede99660e90b6
Benjy Cui <benjytrys@gmail.com>
2016-04-01 15:16:28 +0800

    feat: convert jsonml to React.Component

--- index.js
+++ index.js
@@ -2,9 +2,32 @@
 
 const React = require('react');
 
-const defaultConverters = [
-  [() => true, () => 'hello'],
-];
+const coreToString = Object.prototype.toString;
+function isObject(object) {
+  return coreToString.call(object)
+    .toLowerCase() === '[object object]';
+}
+
+function getTagName(node) {
+  if (typeof node === 'string') return;
+  return node[0];
+}
+
+function getAttributes(node) {
+  if (typeof node === 'string' ||
+      !isObject(node[1])) {
+    return {};
+  }
+  return node[1];
+}
+
+function getChildren(node) {
+  if (typeof node === 'string') {
+    return [];
+  };
+  const start = isObject(node[1]) ? 2 : 1;
+  return node.slice(start);
+}
 
 function cond(data, conds) {
   const pair = conds.find(([pred, _]) => {
@@ -13,6 +36,28 @@ function cond(data, conds) {
   return pair[1](data);
 }
 
-module.exports = function toReactComponent(jsonml, converters=[]) {
-  return cond(jsonml, converters.concat(defaultConverters));
+module.exports = function toReactComponent(converters = [], jsonml) {
+  const defaultConverters = [
+    [(node) => typeof node === 'string', (node) => node],
+    [(node) => getTagName(node) === 'innerHTML', (node) => {
+      return React.createElement('div', {
+        dangerouslySetInnerHTML: {__html: node[1]}
+      });
+    }],
+    [() => true, (node) => {
+      return React.createElement(
+        getTagName(node),
+        getAttributes(node),
+        getChildren(node).map(innerToReactComponent)
+      );
+    }],
+  ];
+
+  const mergeConverters = converters.concat(defaultConverters);
+
+  function innerToReactComponent(jsonml) {
+    return cond(jsonml, mergeConverters);
+  }
+
+  return cond(jsonml, mergeConverters);
 };
