5ab817c71985309093448ee9a44abbbd3d1753f1
Benjy Cui <benjytrys@gmail.com>
2016-05-16 11:17:18 +0800

    test: add unit test stub

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const assert = require('assert');
+const toReactComponent = require('../lib');
+
+describe('jsonml-to-react-component', function() {
+  it('should be OK', function() {
+    assert.ok(true);
+  });
+});
